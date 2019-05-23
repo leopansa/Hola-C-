40b43b6975ec55576a17346e46910c87f52dd783
leopansa <l_paniagua@hotmail.com>
2019-05-23 11:04:54 -0600

    cambio de mensaje

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace hola_mundo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola Mundo!");
         }
     }
 }
